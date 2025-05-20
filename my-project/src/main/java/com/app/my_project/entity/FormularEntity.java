35a148508a9be0109824719eb34dbaf67dfa7321
Meta moveon <poom3753@gmail.com>
2025-05-20 10:35:46 +0700

    page formular

---
+++ my-project/src/main/java/com/app/my_project/entity/FormularEntity.java
@@ -0,0 +1,75 @@
+package com.app.my_project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class FormularEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int qty;
+    private String unit;
+
+    @ManyToOne
+    @JoinColumn(name = "material_id")
+    private MaterialEntity material;
+
+    @ManyToOne
+    @JoinColumn(name = "production_id")
+    private ProductionEntity production;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNmae(String name) {
+        this.name = name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public MaterialEntity getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(MaterialEntity material) {
+        this.material = material;
+    }
+
+    public ProductionEntity getProduction() {
+        return production;
+    }
+
+    public void setProduction(ProductionEntity production) {
+        this.production = production;
+    }
+}
