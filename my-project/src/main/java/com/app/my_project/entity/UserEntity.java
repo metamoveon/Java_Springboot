03fb168fe2e4047c39dee24a042ccc30acbea6db
Meta moveon <poom3753@gmail.com>
2025-06-13 13:47:55 +0700

    update reportapicontroller

--- my-project/src/main/java/com/app/my_project/entity/UserEntity.java
+++ my-project/src/main/java/com/app/my_project/entity/UserEntity.java
@@ -14,6 +14,7 @@ public class UserEntity {
     private String username;
     private String email;
     private String password;
+    private String role;
 
     public Long getId() {
         return id;
@@ -47,4 +48,11 @@ public class UserEntity {
         this.password = password;
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
